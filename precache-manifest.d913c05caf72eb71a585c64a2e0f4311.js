cd139cb22c4ada3784e178740b1aeb0b5b1759f4
ali1129js <ali1129js@gmail.com>
2020-07-27 19:21:11 +0200

    Updates

---
+++ precache-manifest.d913c05caf72eb71a585c64a2e0f4311.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22ad69c8cd55611a5d5ad6262d51e425",
+    "url": "/issuetracker-app/index.html"
+  },
+  {
+    "revision": "f2e98f0e497a1b5b57c1",
+    "url": "/issuetracker-app/static/css/main.cfdd894e.chunk.css"
+  },
+  {
+    "revision": "eda36c0e6c38e65458e2",
+    "url": "/issuetracker-app/static/js/2.8a27eeb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/issuetracker-app/static/js/2.8a27eeb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2e98f0e497a1b5b57c1",
+    "url": "/issuetracker-app/static/js/main.ce94c936.chunk.js"
+  },
+  {
+    "revision": "5e7f16a96360552b543d",
+    "url": "/issuetracker-app/static/js/runtime-main.a5b6f6f1.js"
+  }
+]);
